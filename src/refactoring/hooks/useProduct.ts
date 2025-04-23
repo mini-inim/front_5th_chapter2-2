3788640793324e2b4d424d6b340ddb6d74cd2d32
mini--inim <forestaroundseoul@gmail.com>
2025-04-24 01:27:21 +0900

    useProduct hook 분리

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -2,9 +2,21 @@ import { useState } from "react";
 import { Product } from "../../types.ts";
 
 export const useProducts = (initialProducts: Product[]) => {
+  const [products, setProducts ] = useState<Product[]>(initialProducts)
+
+  const addProduct = (newProduct: Product) => {
+    setProducts(prevProducts => [...prevProducts, newProduct]);
+  }
+
+  const updateProduct = (updatedProduct: Product) => {
+    setProducts(prevProducts =>
+      prevProducts.map(p => p.id === updatedProduct.id ? updatedProduct : p)
+    );
+  }
+
   return {
-    products: [],
-    updateProduct: () => undefined,
-    addProduct: () => undefined,
+    products,
+    updateProduct,
+    addProduct,
   };
 };
